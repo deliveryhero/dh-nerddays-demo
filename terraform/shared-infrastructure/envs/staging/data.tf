d6d2016bbe08387f53975e6bdbd9372f3f4ba76a
Miguel Mingorance <miguel.mingorance@deliveryhero.com>
2020-10-23 10:46:04 +0200

    Adding jumphost

--- terraform/shared-infrastructure/envs/staging/data.tf
+++ terraform/shared-infrastructure/envs/staging/data.tf
@@ -1,1 +1,9 @@
 data "aws_availability_zones" "azs" {}
+
+data "sops_file" "secrets" {
+  source_file = "${path.module}/secrets.enc.yaml"
+}
+
+data "aws_route53_zone" "dh-nerddays-demo" {
+  name = "dh-nerddays-demo.com"
+}
---
+++ terraform/shared-infrastructure/envs/staging/jumphost.tf
@@ -0,0 +1,71 @@
+########################
+# Keypair for Jumphost #
+########################
+
+module "jumphost-key-pair" {
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name   = "jumphost"
+  public_key = data.sops_file.secrets.data["jumphost.public-ssh-key"]
+}
+
+############################
+# EC2 Instance as Jumphost #
+############################
+data "aws_subnet" "eu-central-1a" {
+  availability_zone = "eu-central-1a"
+  vpc_id            = module.vpc.vpc_id
+  tags = {
+    Visibility = "public"
+  }
+  depends_on = [ module.vpc ]
+}
+
+data "aws_ami" "ubuntu-18-04" {
+  owners = ["099720109477"]
+  name_regex = "^ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20201014$"
+}
+
+module "jumphost" {
+  source = "git@github.com:deliveryhero/pd-tf-aws-jumphost?ref=v1.0.2-pre"
+
+  jumphost_instance_type = "t3.medium"
+  ami_id                 = data.aws_ami.ubuntu-18-04.id
+  vpc_id                 = module.vpc.vpc_id
+  az_subnet = {
+    eu-central-1a = data.aws_subnet.eu-central-1a.cidr_block // EBS and Instance must be in the same AZ
+  }
+  external_dns_zone_id        = data.aws_route53_zone.dh-nerddays-demo.id
+  jumphost_keypair            = module.jumphost-key-pair.this_key_pair_key_name
+  env                         = local.tags["Environment"]
+  region                      = local.tags["Region"]
+  monitoring                  = false
+  associate_public_ip_address = true
+  root_block_device = [
+    {
+      volume_size = "20" // in GB
+      volume_type = "gp2"
+    }
+  ]
+
+  ssh_access_port   = 2122
+  ssh_ingress_rules = [
+    {
+      name             = "dh-ips"
+      description      = "Delivery Hero IPs"
+      ipv4_cidr_blocks = module.dh-ips.ipv4_cidr_blocks
+    }
+  ]
+
+  standard_os_users       = ["developer"]
+  sudo_os_users           = ["infra"]
+  packages_to_install     = ["mysql-client", "pv", "redis-tools", "python3", "python3-pip", "python3-dev", "libmysqlclient-dev", "python3-mysql.connector"]
+
+  access_policies = [
+    {
+      name      = "as-infra-user"
+      os_users  = ["infra"]
+      sso_roles = [ for role in yamldecode(data.sops_file.secrets.raw).jumphost.users.infra.sso-roles : role ]
+    }
+  ]
+}
