bc0494815be43ca3036b98648e295b559364e426
Miguel Mingorance <miguel.mingorance@deliveryhero.com>
2020-10-28 00:00:39 +0100

    Testing new way

--- terraform/services/demo-service/mysql.tf
+++ terraform/services/demo-service/mysql.tf
@@ -10,7 +10,8 @@ resource "mysql_user" "demo_service" {
 
 resource "mysql_user" "test_demo_service" {
   provider           = mysql.demo-service
-  for_each           = data.sops_file.secrets.data["test.users"]
+  for_each           = [ for user in yamldecode(data.sops_file.secrets.raw).test.users : user ]
+//  for_each           = data.sops_file.secrets.data["test.users"]
   user               = each.value.username
   plaintext_password = each.value.password
   host               = each.value.host
@@ -26,7 +27,8 @@ resource "mysql_grant" "demo_service" {
 
 resource "mysql_grant" "test_demo_service" {
   provider   = mysql.demo-service
-  for_each   = data.sops_file.secrets.data["test.users"]
+  for_each   = [ for user in yamldecode(data.sops_file.secrets.raw).test.users : user ]
+//  for_each   = data.sops_file.secrets.data["test.users"]
   user       = each.value.username
   host       = each.value.host
   database   = each.value.privileges.database
@@ -43,6 +45,7 @@ resource "mysql_database" "demo_service" {
 
 resource "mysql_database" "test_demo_service" {
   provider = mysql.demo-service
-  for_each = [ for database in data.sops_file.secrets.data["test.databases"] : database ]
+  for_each = [ for database in yamldecode(data.sops_file.secrets.raw).test.databases : database ]
+//  for_each = [ for database in data.sops_file.secrets.data["test.databases"] : database ]
   name     = each.value
 }
