248397c02796a8c5fc7203bff1d0b27abf9741f6
Miguel Mingorance <miguel.mingorance@deliveryhero.com>
2020-10-27 23:56:27 +0100

    Testing new way

--- terraform/services/demo-service/mysql.tf
+++ terraform/services/demo-service/mysql.tf
@@ -8,12 +8,12 @@ resource "mysql_user" "demo_service" {
   host               = data.sops_file.secrets.data["aurora-mysql.users.demo-service.host"]
 }
 
-###################
-# MySQL Databases #
-###################
-resource "mysql_database" "demo_service" {
-  provider = mysql.demo-service
-  name     = data.sops_file.secrets.data["aurora-mysql.databases.demo-service.name"]
+resource "mysql_user" "test_demo_service" {
+  provider           = mysql.demo-service
+  for_each           = data.sops_file.secrets.data["test.users"]
+  user               = each.value.username
+  plaintext_password = each.value.password
+  host               = each.value.host
 }
 
 resource "mysql_grant" "demo_service" {
@@ -22,4 +22,27 @@ resource "mysql_grant" "demo_service" {
   host       = mysql_user.demo_service.host
   database   = mysql_database.demo_service.name
   privileges = [ for privilege in yamldecode(data.sops_file.secrets.raw).aurora-mysql.privileges.users.demo-service.databases.demo-service : privilege ]
+}
+
+resource "mysql_grant" "test_demo_service" {
+  provider   = mysql.demo-service
+  for_each   = data.sops_file.secrets.data["test.users"]
+  user       = each.value.username
+  host       = each.value.host
+  database   = each.value.privileges.database
+  privileges = [ for privilege in each.value.privileges.grants : privilege ]
+}
+
+###################
+# MySQL Databases #
+###################
+resource "mysql_database" "demo_service" {
+  provider = mysql.demo-service
+  name     = data.sops_file.secrets.data["aurora-mysql.databases.demo-service.name"]
+}
+
+resource "mysql_database" "test_demo_service" {
+  provider = mysql.demo-service
+  for_each = [ for database in data.sops_file.secrets.data["test.databases"] : database ]
+  name     = each.value
 }
