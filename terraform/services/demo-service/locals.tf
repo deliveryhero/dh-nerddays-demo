4ed9d0e0fd049be4672ec5a543c58a9a143a2495
Miguel Mingorance <miguel.mingorance@deliveryhero.com>
2020-10-28 11:52:59 +0100

    Remove RDS

--- terraform/services/demo-service/locals.tf
+++ terraform/services/demo-service/locals.tf
@@ -1,10 +1,4 @@
 locals {
-  mysql_provider = {
-    endpoint = format("%s:3306", module.aurora_mysql.this_rds_cluster_endpoint)
-    username = data.sops_file.secrets.data["aurora-mysql.root-credentials.user"]
-    password = data.sops_file.secrets.data["aurora-mysql.root-credentials.password"]
-  }
-
   tags = {
     Environment = "demo"
     Region      = "eu"
