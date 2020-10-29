0e7dd775d4100efd831c9621b693c02cb107314a
Miguel Mingorance <miguel.mingorance@deliveryhero.com>
2020-10-29 12:41:11 +0100

    Updates

--- terraform/services/demo-service/provider.tf
+++ terraform/services/demo-service/provider.tf
@@ -8,28 +8,9 @@ terraform {
     region  = "eu-central-1"
     profile = "dh-nerddays-demo"
   }
-
-  required_providers {
-    sops = {
-      source  = "carlpett/sops"
-      version = "0.5.2"
-    }
-    mysql = {
-      source  = "terraform-providers/mysql"
-      version = "1.9.0"
-    }
-  }
 }
 
 provider "aws" {
   profile = "dh-nerddays-demo"
   region  = "eu-central-1"
-}
-
-provider "sops" {}
-
-provider "mysql" {
-  endpoint = local.mysql_provider.endpoint
-  username = local.mysql_provider.username
-  password = local.mysql_provider.password
 }
