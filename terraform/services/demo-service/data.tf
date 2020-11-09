b20d123871bf8100afb9415a5debbac1d965b9d3
Miguel Mingorance <miguel.mingorance@deliveryhero.com>
2020-11-09 22:08:17 +0100

    Readd changes

--- terraform/services/demo-service/data.tf
+++ terraform/services/demo-service/data.tf
@@ -1,6 +1,10 @@
+data "sops_file" "secrets" {
+  source_file = "${path.module}/secrets.enc.yaml"
+}
+
 data "aws_availability_zones" "azs" {}
 
-data "aws_route53_zone" "internal-dh-nerddays-demo" {
+data "aws_route53_zone" "dh-nerddays-demo" {
   name         = "internal.dh-nerddays-demo.com"
   private_zone = true
 
